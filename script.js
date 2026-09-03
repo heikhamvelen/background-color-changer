9379e4368f3b47d489b304aec36900675f0c6a3c
itsVelenHeikham <itsvelentinaheikham@gmail.com>
2026-09-03 23:27:01 +0530

    Update background color changer

--- script.js
+++ script.js
@@ -4,18 +4,14 @@ const blueBtn = document.querySelector('#blueBtn');
 const yellowBtn = document.querySelector('#yellowBtn');
 const randomBtn = document.querySelector('#randomBtn');
 const heading = document.querySelector('#title');
-redBtn.addEventListener('click', function(){
-    document.body.style.backgroundColor = 'red';
-});
-greenBtn.addEventListener('click', function(){
-    document.body.style.backgroundColor = 'green';
-});
-blueBtn.addEventListener('click', function(){
-    document.body.style.backgroundColor = 'blue';
-});
-yellowBtn.addEventListener('click', function(){
-    document.body.style.backgroundColor = 'yellow';
-});
+
+const colorButtons = [redBtn, greenBtn, blueBtn, yellowBtn];
+const buttonColors = ['red', 'green', 'blue', 'yellow'];
+for (let i = 0; i < colorButtons.length; i++) {
+    colorButtons[i].addEventListener('click', function(){
+        document.body.style.backgroundColor = buttonColors[i];
+    });
+}
 const colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink'];
 randomBtn.addEventListener('click', function(){
     const randomIndex=Math.floor(Math.random()*colors.length);
